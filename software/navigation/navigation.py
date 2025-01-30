b645c67e0329b8a2b000c918112097cc46f976dc
Sean Lim <sl2185@cam.ac.uk>
2025-01-30 09:11:05 +0000

    robot class

---
+++ software/navigation/navigation.py
@@ -0,0 +1,6 @@
+from graph import Graph, initialise_graph
+
+class Navigation:
+    def __init__(self):
+        self.graph = initialise_graph()
+        
--- software/robot.py
+++ software/robot.py
@@ -5,17 +5,21 @@ from controllers.linear_actuator_driver import LinearActuatorDriver
 from pid.pid import LineFollower
 from sensors.sensor_manager import LineSensor, CrashSensor, UltraSound
 from sensors.code_reader import CodeReader
+from collections import deque
+from time import time , sleep
+
 class Robot :
     def __init__(self):
         '''
-        Inititalise all sensors, motors, linefollower class
+        Inititalise all sensors, motors, linefollower class done
         - 4 line sensors (inside PID class anyway)
-        - 1 dual motor driver done
+        - 1 dual motor driver done`
         - 1 ultrasonic sensor done
         - 1 qr code scanner done
-        - 1 linear actuator
-        - 1 led 
-        - 1 push bottom (start/stop/reset)
+        - 1 linear actuator done
+        - 1 led done
+        - 1 push bottom (start/stop/reset) done
+        - initialise navigation and graph
 
         Robot variables + states:
         - Current task (idle,line following forward, reversing, turning left/right, picking up box, dropping box, scanning qr code) 
@@ -26,8 +30,12 @@ class Robot :
         - Time elapsed
         - Current speed % (0-100)
         - Boxes in each depot (4,3,2,1,0)
-        - Robot running (True/False)
+
+        robot path should be a list of tuples 
         '''
+        # Initialise LED and button
+        self.led = Pin(14, Pin.OUT)
+        self.button = Pin(12, Pin.IN, Pin.PULL_DOWN)
 
         # Initialising Motors and actuators
         self.dual_motors = DualMotor(left_dir_pin=6, left_pwm_pin=7, right_dir_pin=5, right_pwm_pin=4)
@@ -43,6 +51,39 @@ class Robot :
         self.outer_right_sensor = LineSensor("""add pins""")
 
         # Init line follower
+        self.line_follower = LineFollower(None, self.outer_left_sensor, self.inner_left_sensor, self.inner_right_sensor, self.outer_right_sensor,"""add kp,ki ,kd""")
+
+        # Init navigaton
+        # TODO
+
+        # Robot variables and states
+        self.current_task = "idle"
+        self.direction_facing = 1
+        self.current_position = "Start"
+        self.goal_position = "Depot 1"
+        self.next_target_position = "Depot 1"
+        self.time_elapsed = 0
+        self.current_speed = 0
+        self.boxes_in_depot = {"Depot 1":4, "Depot 2":4}
+        self.depot_loop_running = False
+        self.robot_path = [] # deque of tuples containing directions of travel and number of nodes to pass (1,1)
+
+    def robot_standby(self):
+        while True:
+            if self.button.value() == 1:
+                self.start()
+                break
+    
+    def start(self):
+        raise NotImplementedError
+    
+    def return_to_start(self):
+        raise NotImplementedError
+    
+    def goto_node(self,target_node):
+
+        raise NotImplementedError
+
         
 
 
