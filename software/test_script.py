e4faf51640d73ba09e786bef4154d2ee0f7a7a5c
Sean Lim <sl2185@cam.ac.uk>
2025-01-31 11:15:31 +0000

    added tests  for starting, turning, line following and navigation

--- software/test_script.py
+++ software/test_script.py
@@ -1,16 +1,33 @@
 from robot import Robot
 
 
-def test_script():
-    # Test the robot by going to depot 1 then returning to start 
-    # Initialise robot object
-     
-    wilsonbot = Robot()
-    
+def test_start(wilsonbot):
+    # Test the robot to go to the start node
     # Start the robot on standby mode
     # Will start when button is pressed
     wilsonbot.robot_standby()
 
+    # robot should be at the start node
+
+def test_line_following(wilsonbot):
+
+    # Test line following by moving along the line for 2 junctions
+    wilsonbot.move(2, 30)
+
+def test_turning(wilsonbot):
+    # Test 90 degree turn
+    wilsonbot.face_direction(2)
+
+    # Test 180 degree turn
+    wilsonbot.face_direction(4)
+
+    # test 90 degree turn
+    wilsonbot.face_direction(3)
+
+    # Go back to start
+    wilsonbot.face_direction(1)
+
+def test_navigation(wilsonbot):
     print("STARTING")
 
     # Go to the first depot
@@ -21,9 +38,13 @@ def test_script():
     # Return to the start node
     wilsonbot.return_to_start()
 
-    # Print out robot variables for debugging
-
-
 if __name__ == "__main__":
-    test_script()
+    # Init robot 
+    wilsonbot = Robot()
+    test_start(wilsonbot)
+
+    test_line_following(wilsonbot)
+    
+    #test_turning(wilsonbot)
+    #test_navigation(wilsonbot)
     print("test complete")
