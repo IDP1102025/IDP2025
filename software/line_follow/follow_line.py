abfd9ec869e7524126a73ebc1dd20d6d50eb9263
Sean Lim <sl2185@cam.ac.uk>
2025-02-05 15:31:58 +0000

    tuned weights for follow line

--- software/line_follow/follow_line.py
+++ software/line_follow/follow_line.py
@@ -1,4 +1,4 @@
-import time
+from time import sleep
 from sensors.sensor_manager import LineSensor
 
 class NotPidFollower:
@@ -8,7 +8,7 @@ class NotPidFollower:
         self.inner_left_sensor = inner_left_sensor
         self.inner_right_sensor = inner_right_sensor
         self.outer_right_sensor = outer_right_sensor
-
+        self.state_pattern = []
         self.error_loop = 0
 
     def follow_the_line(self, current_left_speed, current_right_speed):
@@ -19,32 +19,35 @@ class NotPidFollower:
         outer_right_detect = self.outer_right_sensor.read_sensor()
 
         # 2) Determine sensor pattern
-        state_pattern = [outer_left_detect,
+        self.state_pattern = [outer_left_detect,
                          inner_left_detect,
                          inner_right_detect,
                          outer_right_detect]
         
-        if state_pattern == [0,1,1,0]:
-            self.error_loop += 1
-            current_left_speed, current_right_speed = max(current_right_speed,current_left_speed)
-            if self.error_loop >= 10:
-                current_left_speed += 5
-                current_right_speed += 5
-            return current_left_speed, current_right_speed
-        elif state_pattern in ([0,0,1,0], [0,1,0,0], [0,1,0,1], [1,0,1,0]):
-            return current_left_speed, current_right_speed
+        if self.state_pattern == [0,1,1,0]:
+            self.error_loop += 1 # Add 1 to the straight counter
+            # Set speeds to match
+            if self.error_loop >= 15:
+                current_left_speed = 85
+                current_right_speed = 85
+            else:
+                current_left_speed = 80
+                current_right_speed = 80
         else:
             self.error_loop = 0
-            if state_pattern == [0,0,1,1]:  # slightly left
-                current_left_speed += 5
-                current_right_speed -= 5
-            elif state_pattern == [1,1,0,0]:   # slightly right
-                current_left_speed -= 5
-                current_right_speed += 5
-            elif state_pattern == [0,0,0,1]: # bigger correction
-                current_left_speed += 10
-                current_right_speed -= 10        
-            elif state_pattern == [1,0,0,0]:
-                current_left_speed -= 10
-                current_right_speed += 10 
-            return current_left_speed, current_right_speed
+            if self.state_pattern == [0,0,1,1]:  # slightly left
+                current_left_speed =  80
+                current_right_speed = 70
+            elif self.state_pattern == [1,1,0,0]:   # slightly right
+                current_left_speed = 70
+                current_right_speed = 80
+            elif self.state_pattern == [0,0,0,1]: # bigger correction
+                current_left_speed = 80
+                current_right_speed = 60
+            elif self.state_pattern == [1,0,0,0]:
+                current_left_speed = 60
+                current_right_speed = 80
+        return current_left_speed, current_right_speed
+            
+
+
